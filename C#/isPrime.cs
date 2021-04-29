283b790477ba7131e59850ffde30bc38c28ae830
Surafel <se.surafel.kassahun@gmail.com>
2021-04-29 08:38:39 +0300

    Update isPrime.cs

--- C#/isPrime.cs
+++ C#/isPrime.cs
@@ -1,9 +1,11 @@
-#include <stdbool.h>
-
-bool is_prime(int num)
+public static class Kata
 {
-  if (num <= 1) return false;   
-  for (int i = 2; i*i <= num; i++) {
-    if (num % i == 0) return false;
-  } return true;
+  public static bool IsPrime(int n)
+  {
+    // TODO
+    if (n <= 1) return false;
+    for (int i = 2; i*i <= n; i++) {
+      if (n % i == 0) return false;
+    } return true;
+  }
 }
